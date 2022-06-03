56d992b61548b5edb3733806b99db35c27fc85cd
abdulnasir628 <muhammadabdulnasir48@gmail.com>
2022-06-03 21:36:11 +0700

    create cf.html && cf.js

---
+++ cf.js
@@ -0,0 +1,23 @@
+let nilai = prompt("Input nilai: ");
+
+    if (nilai >= 80 && nilai <= 100) {
+      alert("Nilai anda A");
+    } else if (nilai >= 60 && nilai < 80) {
+      alert("Nilai anda B");
+    } else if (nilai >= 40 && nilai < 60) {
+      alert("Nilai anda C");
+    } else if (nilai >= 20 && nilai < 60) {
+      alert("Nilai anda D");
+    } else {
+      alert("Nilai anda E");
+    }
+
+    console.log(nilai);
+
+    if (nilai < 60) {
+      alert(nilai + ' harap belajar lebih rajin ')
+    } else {
+      alert(nilai + ' pertahankan dan tingkatkan ') 
+    }
+
+    console.log(nilai);
